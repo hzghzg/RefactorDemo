6d983f4e3903c3b19662d7c5b92337ce30e3d4a8
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-08-12 22:07:10 +0800

    test:add test should_return_0_and_50_when_updateQuality_given_Aged_Brie_its_sellIn_is_1_quality_is_49

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -185,4 +185,14 @@ public class GildedRoseTest {
         assertEquals(-1,item.sellIn);
         assertEquals(50, item.quality);
     }
+
+    @Test
+    public void should_return_0_and_50_when_updateQuality_given_Aged_Brie_its_sellIn_is_1_quality_is_49(){
+        Item item = new Item("Aged Brie", 1, 49);
+        Item[] items = {item};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(0,item.sellIn);
+        assertEquals(50, item.quality);
+    }
 }
