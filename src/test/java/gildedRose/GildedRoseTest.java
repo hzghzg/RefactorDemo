5f1297db9eb95221916c1e0b21651e55fd9f3bff
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-08-12 20:34:50 +0800

    test:add test should_return_13_and_minus_1_when_updateQuality_given_small_cake_its_sellIn_is_0_quality_is_15

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 public class GildedRoseTest {
     @Test
-    public void should_return_14_when_updateQuality_given_a_normal_item_its_sellIn_is_15_quality_is_15(){
+    public void should_return_14_when_updateQuality_given_small_cake_its_sellIn_is_15_quality_is_15(){
         Item item = new Item("small cake", 15, 15);
         Item[] items = {item};
         GildedRose gildedRose = new GildedRose(items);
@@ -15,12 +15,21 @@ public class GildedRoseTest {
         assertEquals(14,item.sellIn);
     }
     @Test
-    public void should_return_14_and_0_when_updateQuality_given_a_normal_item_its_sellIn_is_15_quality_is_0(){
+    public void should_return_14_and_0_when_updateQuality_given_small_cake_its_sellIn_is_15_quality_is_0(){
         Item item = new Item("small cake", 15, 0);
         Item[] items = {item};
         GildedRose gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
         assertEquals(0, item.quality);
         assertEquals(14,item.sellIn);
+    }
+    @Test
+    public void should_return_13_and_minus_1_when_updateQuality_given_small_cake_its_sellIn_is_0_quality_is_15(){
+        Item item = new Item("small cake", 0, 15);
+        Item[] items = {item};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(13, item.quality);
+        assertEquals(-1,item.sellIn);
     }
 }
