0c4365d7c956cc41bd9d5b4281f98c98e04196d4
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-08-12 21:42:57 +0800

    test:add test should_return_0_and_50_when_updateQuality_given_Backstage_its_sellIn_is_1_quality_is_48

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -122,4 +122,13 @@ public class GildedRoseTest {
         assertEquals(-1,item.sellIn);
         assertEquals(0, item.quality);
     }
+    @Test
+    public void should_return_0_and_50_when_updateQuality_given_Backstage_its_sellIn_is_1_quality_is_48(){
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 1, 50);
+        Item[] items = {item};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(0,item.sellIn);
+        assertEquals(50, item.quality);
+    }
 }
