5749b2a44b9134a198e1182be265c3d9355a0a1f
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-08-13 12:43:43 +0800

    refactor:remove duplicate code in Backstage

--- src/main/java/gildedRose/Backstage.java
+++ src/main/java/gildedRose/Backstage.java
@@ -3,43 +3,13 @@ package gildedRose;
 public class Backstage implements ItemStratedy {
     @Override
     public void update(Item item) {
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0&&!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.quality--;
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")&&item.quality < 50) {
-                    if (item.sellIn < 11) {
-                        item.quality++;
-                    }
-                    if (item.sellIn < 6) {
-                        item.quality++;
-                    }
-                }
-            }
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+                if (item.sellIn < 11&&item.quality < 50) item.quality++;
+                if (item.sellIn < 6&&item.quality < 50) item.quality++;
         }
 
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
-
-        if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0&&!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality--;
-                    }
-                } else {
-                    item.quality = 0;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality ++;
-                }
-            }
-        }
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) item.quality = 0;
     }
 }
